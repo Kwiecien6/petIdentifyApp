26a8e3055884963ffe2dddbe19f8fdb6b4ede70d
jyq <896836176@qq.com>
2020-11-22 01:43:37 +0800

    3.OpenCV环境配置成功，图像灰度转变测试OK

--- app/src/main/java/com/jyq/petidentifyapp/MainActivity.java
+++ app/src/main/java/com/jyq/petidentifyapp/MainActivity.java
@@ -2,27 +2,69 @@ package com.jyq.petidentifyapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 import org.opencv.android.OpenCVLoader;
+import org.opencv.android.Utils;
+import org.opencv.core.CvType;
+import org.opencv.core.Mat;
+import org.opencv.imgproc.Imgproc;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String CV_TAG = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        iniLoadOpenCV();
+        Button gray = findViewById(R.id.button);
+        gray.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                convert2Gray();
+            }
+        });
     }
 
-    @Override
-    public void onResume() {
 
-        super.onResume();
-        if(!OpenCVLoader.initDebug()){
-            Log.i("cv", "Internal OpenCV library not found. Using OpenCV Manager for initialization");
-        } else {
-            Log.i("cv", "OpenCV library found inside package. Using it!");
+    private void iniLoadOpenCV(){
+        boolean success = OpenCVLoader.initDebug();
+        if(success){
+            Toast.makeText(this.getApplicationContext(),
+                    "OpenCV Libraries loaded succeed!",
+                    Toast.LENGTH_SHORT).show();
+        }else{
+            Toast.makeText(this.getApplicationContext(),
+                    "WARNING:Could not load OpenCV Libraries!",
+                    Toast.LENGTH_SHORT).show();
         }
     }
+
+
+    //调节图像灰度（功能测试）
+    private void convert2Gray(){
+        Bitmap image = BitmapFactory.decodeResource(getResources(),R.drawable.samplepic);
+        Mat src = new Mat();
+        Mat dst = new Mat();
+        Utils.bitmapToMat(image,src);//把image转化为Mat
+        Imgproc.cvtColor(src,dst,Imgproc.COLOR_BGRA2GRAY);//这里由于使用的是Imgproc这个模块所有这里要这么写
+        Utils.matToBitmap(dst,image);//把mat转化为bitmap
+        ImageView imageView = findViewById(R.id.imgView);
+        imageView.setImageBitmap(image);
+        //release
+        src.release();
+        dst.release();
+    }
+
 }
