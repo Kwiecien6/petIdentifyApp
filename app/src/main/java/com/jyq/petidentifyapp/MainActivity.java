c1cf14fccc8dba9ddf8f15942f30f05622d7809a
jyq <896836176@qq.com>
2020-11-21 19:28:08 +0800

    2.导入openCV SDK，结果待测试

--- app/src/main/java/com/jyq/petidentifyapp/MainActivity.java
+++ app/src/main/java/com/jyq/petidentifyapp/MainActivity.java
@@ -3,6 +3,9 @@ package com.jyq.petidentifyapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import org.opencv.android.OpenCVLoader;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void onResume() {
+
+        super.onResume();
+        if(!OpenCVLoader.initDebug()){
+            Log.i("cv", "Internal OpenCV library not found. Using OpenCV Manager for initialization");
+        } else {
+            Log.i("cv", "OpenCV library found inside package. Using it!");
+        }
     }
 }
